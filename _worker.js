240e34c7d5259ea815be94df57356dd040bf7293
Peap <123750838+PeapGit@users.noreply.github.com>
2025-11-16 10:35:22 -0700

    WELL

--- _worker.js
+++ _worker.js
@@ -1,24 +1,23 @@
 export default {
-    async fetch(request, env, ctx) {
+    async fetch(request, env) {
         const url = new URL(request.url);
-
+        
         if (url.pathname === "/assets/images/logo.png") {
             const now = new Date();
-            const month = now.getUTCMonth() + 1;  // 1–12
+            const month = now.getUTCMonth() + 1;
             const day = now.getUTCDate();
 
             const isChristmas =
-                (month === 11) ||
-                (month === 12);
+                month === 11 ||
+                month === 12;
 
             const file = isChristmas
-                ? "assets/images/christmas.png"
-                : "assets/images/normal.png";
+                ? "/assets/images/christmas.png"
+                : "/assets/images/normal.png";
 
-            return env.ASSETS.fetch("https://fake/" + file);
+            return env.ASSETS.fetch("https://fake" + file);
         }
 
-        // Everything else served normally
         return env.ASSETS.fetch(request);
     }
 };
